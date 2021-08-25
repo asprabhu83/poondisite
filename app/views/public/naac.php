437f0526d4c38be95a07ca3c9cacefa47b1e6db8
Manikandan R <mani_suja@hotmail.com>
2021-08-25 11:05:40 +0530

    link update3

--- app/views/public/naac.php
+++ app/views/public/naac.php
@@ -51,7 +51,7 @@ $token=$_SESSION['name'];
                             <div>1.1.1 POs, PSOs and COS  
 								<!-- <span>20</span> -->
                                 <!-- <a href="/assets/Naac Doc/Link_Quality_Indicator_Framework.pdf" target="_blank">View Document</a> -->
-                                <a href="/assets/NAAC/1.1.1_Front_Page.pdf" target="_blank"><img src="assets/images/pdf_icon1.jpg" alt="" width="70px" /></a>
+                                <a href="/assets/NAAC/1.1.1_Front_Page.pdf" target="_blank"><img src="assets/images/pdf_icon2.png" alt="" width="70px" /></a>
                             
 							</div>
                             <div>1.1.2 Syllabus Revision <span>20</span>
