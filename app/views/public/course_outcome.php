88ec32d085bb572512a42cd0c13c7b894b318094
ranjithgee <ranjithgee968@gmail.com>
2021-09-03 15:26:21 +0530

    add pdfs

--- app/views/public/course_outcome.php
+++ app/views/public/course_outcome.php
@@ -15,48 +15,48 @@ init_public_head();
                         <div class="content_item content_item1 active">
                             <div class="ug_courses">
                                 <div class="head">UG Programme</div>
-                                <div>B.A. History</div>
-                                <div>B.A. Economics</div>
-                                <div>B.A. Tamil</div>
-                                <div>B.A. Indian Culture</div>
-                                <div>B.Lit. Tamil</div>
-                                <div>B.A. English</div>
-                                <div>B.Com.</div>
-                                <div>B. Com. Computer Applications</div>
-                                <div>B. Com. Banking Management</div>
-                                <div>B.Sc. Mathematics</div>
-                                <div>B.Sc. Physics</div>
-                                <div>B.Sc. Chemistry</div>
-                                <div>B.Sc. Botany</div>
-                                <div>B.Sc. Zoology</div>
-                                <div>B.Sc. Computer Science</div>
-                                <div>B.C.A. Computer Applications</div>
-                                <div>B.Sc. Physical Education, Health Education and Sports</div>   
-                                <div>B.B.A. Business Administration</div>
-                                <div>B.Sc. Statistics</div>
-                                <div>B.Sc. Geography</div>
+                                <div><a href="/pdf/UG_courses/B.A_History_course_outcome.pdf"  target="_blank">B.A. History</a></div>
+                                <div><a href="/pdf/UG_courses/B.A_Economics_course_outcome.pdf" target="_blank">B.A. Economics</a></div>
+                                <div><a href="/pdf/UG_courses/B.A_Tamil_course_outcome.pdf" target="_blank">B.A. Tamil</a></div>
+                                <div><a href="/pdf/UG_courses/B.A_Indianculture_course_outcome.pdf" target="_blank">B.A. Indian Culture</a></div>
+                                <div><a href="/pdf/UG_courses/B.Lit_Tamil_course_outcome.pdf" target="_blank">B.Lit. Tamil</a></div>
+                                <div><a href="/pdf/UG_courses/B.A_English_course_outcome.pdf" target="_blank">B.A. English</a></div>
+                                <div><a href="/pdf/UG_courses/B.Com_course_outcome.pdf" target="_blank"> B.Com.</a></div>
+                                <div><a href="/pdf/UG_courses/B.Com_Computerapplication_course_outcome.pdf" target="_blank"> B.Com. Computer Applications</a></div>
+                                <div><a href="/pdf/UG_courses/B.Com_Bankmanagement_course_outcome.pdf" target="_blank"> B.Com. Banking Management</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Mathematics_course_outcome.pdf" target="_blank"> B.Sc. Mathematics</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Physics_course_outcome.pdf" target="_blank"> B.Sc. Physics</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Chemistry_course_outcome.pdf" target="_blank"> B.Sc. Chemistry</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Botany_course_outcome.pdf" target="_blank">B.Sc. Botany</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Zoology_course_outcome.pdf" target="_blank"> B.Sc. Zoology</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Computerscience_course_outcome.pdf" target="_blank"> B.Sc. Computer Science</a></div>
+                                <div><a href="/pdf/UG_courses/B.C.A_course_outcome.pdf" target="_blank"> B.C.A. Computer Applications</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Physicaleducation_course_outcome.pdf" target="_blank"> B.Sc. Physical Education, Health Education and Sports</a></div>   
+                                <div><a href="/pdf/UG_courses/B.B.A_course_outcome.pdf" target="_blank"> B.B.A. Business Administration</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Statistics_course_outcome.pdf" target="_blank"> B.Sc. Statistics</a></div>
+                                <div><a href="/pdf/UG_courses/B.Sc_Geography_course_outcome.pdf" target="_blank">B.Sc. Geography</a> </div>
                             </div>
                         </div>
                         <div class="content_item content_item2">
                               <div class="pg_courses">
                                     <div class="head">PG Programme</div>
-                                    <div>M.A. History</div>
-                                    <div>M.A. Economics</div>
-                                    <div>M.A. Tamil</div>
-                                    <div>M.A. English</div>
-                                    <div>M.Com. </div>
-                                    <div>M.Sc. Mathematics</div>
-                                    <div>M.Sc. Physics</div>
-                                    <div>M.Sc. Chemistry</div>
-                                    <div>M.Sc. Botany</div>
-                                    <div>M.Sc. Microbiology</div>
-                                    <div>M.Sc. Zoology</div>
-                                    <div>M.Sc. Biotechnology</div>
-                                    <div>M.Sc. Computer Science</div>
-                                    <div>M.C.A. Computer Applications</div>
-                                    <div>M.Sc. Information Technology</div>
-                                    <div>M.L.I.S</div>
-                                    <div>M.B.A. Business Administration</div>
+                                    <div><a href="/pdf/PG_courses/M.A_History_course_outcome.pdf" target="_blank">M.A. History</a></div>
+                                    <div><a href="/pdf/PG_courses/M.A_Economics_course_outcome.pdf" target="_blank">M.A. Economics</a></div>
+                                    <div><a href="/pdf/PG_courses/M.A_Tamil_course_outcome.pdf" target="_blank">M.A. Tamil</a></div>
+                                    <div><a href="/pdf/PG_courses/M.A_English_course_outcome.pdf" target="_blank">M.A. English</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Com_course_outcome.pdf" target="_blank">M.Com. </a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Mathematics_course_outcome.pdf" target="_blank">M.Sc. Mathematics</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Physics_course_outcome.pdf" target="_blank">M.Sc. Physics</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Chemistry_course_outcome.pdf" target="_blank">M.Sc. Chemistry</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Botany_course_outcome.pdf" target="_blank">M.Sc. Botany</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Microbiology_course_outcome.pdf" target="_blank">M.Sc. Microbiology</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Zoology_course_outcome.pdf" target="_blank">M.Sc. Zoology</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Biotechnology_course_outcome.pdf" target="_blank">M.Sc. Biotechnology</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Computerscience_course_outcome.pdf" target="_blank">M.Sc. Computer Science</a></div>
+                                    <div><a href="/pdf/PG_courses/M.C.A_course_outcome.pdf" target="_blank">M.C.A. Computer Applications</a></div>
+                                    <div><a href="/pdf/PG_courses/M.Sc_Informationtechnology_course_outcome.pdf" target="_blank">M.Sc. Information Technology</a></div>
+                                    <div><a href="/pdf/PG_courses/M.L.I.S_course_outcome.pdf" target="_blank">M.L.I.S</a></div>
+                                    <div><a href="/pdf/PG_courses/M.B.A_course_outcome.pdf" target="_blank">M.B.A. Business Administration</a></div>
                                 </div>
                         </div>
                 </div>
