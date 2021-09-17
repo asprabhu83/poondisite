41cae30aeafa532c911805c4f434ea8f383dad40
Manikandan R <mani_suja@hotmail.com>
2021-09-17 15:11:09 +0530

    obc updated

---
+++ app/views/public/sc_st_cell1.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+init_public_head();
+?>
+
+<!------------------------------------------------------------------------------------------------>
+<section class="section-90 section-md-111 text-left">
+    <div class="shell">
+        <h2><strong>SC & ST CELL</strong></h2>
+        <br />
+
+        <h5><strong>Ref.No :</strong></h5> <br />
+        <ul>
+            <li>SC & ST Cell/03/85/2020<br /></li>
+        </ul>
+        <br>
+            <h5><strong>Cell Coordinator :</strong></h5></br>
+            <h6><a href="<?php echo base_url('public/pdf/Departments/english/AIDED/Eng.%204.%20Dr.%20K.%20Sivakumar.pdf') ?>" target="_blank" rel="noopener">Dr. K. Sivakumar</a></h6>
+            <br /> <p>Associate Professor</p>
+            <p>Department of English</p>
+            <p>AVVM Sri Pushpam College, Poondi - 613503.</p>
+            <p> Contact No:<strong>9715304677</strong></p>
+        <br>
+    </div>
+</section>
+<?php
+init_public_tail();
+?>
