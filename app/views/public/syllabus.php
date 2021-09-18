d93827bc1c6896dfa331fbee37f5165eb4ca0138
Manikandan R <mani_suja@hotmail.com>
2021-09-18 14:58:06 +0530

    minority cell updated

---
+++ app/views/public/syllabus.php
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+                        <div class="handing-divider">
+							<div class="cell-sm-10 cell-md-11 cell-lg-9 cell-xl-6 offset-top-30">
+								<div>
+									<h4 class="text-bold center">SYLLABUS</h4>
+									<div class="offset-top-20">
+										<ul class="inset-left-30 list list-marked">
+											<li> <?php syllabus_link('B.A Tamil ', 'Tamil/batamil') ?></li>
+											<li> <?php syllabus_link('B.A Indian Culture', 'Tamil/baindianculture') ?></li>
+											<li> <?php syllabus_link('M.A Tamil', 'Tamil/matamil') ?></li>
+											<li> <?php syllabus_link('B.Lit', 'Tamil/blit') ?></li>
+
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+                        
