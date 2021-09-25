77bf8e055f43eb840a36fee1a182dfd81ee7700e
Manikandan R <mani_suja@hotmail.com>
2021-09-25 16:34:18 +0530

    syllabus2007

--- app/views/public/syllabus_2017.php
+++ app/views/public/syllabus_2017.php
@@ -5,7 +5,7 @@ init_public_head();
 
 <main class="syllabus_2007">
     <div class="container">
-        <div class="heading">Syllabus - 2007</div>
+        <div class="heading">Syllabus 2007</div>
         <div class="table_contents" style="overflow-x:auto;">
             <table>
                 <thead>
