dd3cec79c880c394ddb4c719d6dbe834f180b860
Manikandan R <mani_suja@hotmail.com>
2021-09-27 17:18:04 +0530

    certificate updated

---
+++ app/views/public/certificate.php
@@ -0,0 +1,147 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+init_public_head();
+?>
+<div class="container-fluid marqueewrap">
+	<div class="alert alert-primary alert-dismissible" role="alert">
+		<marquee>
+			<strong><span>&#9733;</span> <a href="<?php echo base_url('https://cubonline.biz/StudentLogin?Instname=AVVMSPCP') ?>" target="_blank">Last date for Payment of Tuition Fees on 28.09.2021 at 4.30 p.m.</a><span>&#9733;</span></strong>
+			<!-- <strong>35th Academic Council Meeting on 18.09.2021 at 10.30 a.m.</strong><span>&#9733;</span> -->
+			<!-- <strong><a href="<?php echo base_url('assets/images/founderdaysep2021.jpeg') ?>" target="_blank">51st Founder's Commemoration Day on 17.09.2021</strong></a><span>&#9733;</span> -->
+			<!-- <strong><a href="<?php echo base_url('assets/images/iobpay.jpeg') ?>" target="_blank">Inaugural Function of IOB Pay on 14.09.2021 at 2.30 p.m.</strong></a> -->
+		</marquee>
+	</div>
+</div>
+<!-- Home 01 Banner Starts -->
+<div id="wowslider-container1">
+	<div class="ws_images">
+		<ul>
+			
+		<li><img src="<?php echo base_url('assets/images/banner/002.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/003.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/004.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/005.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/006.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/007.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/008.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/009.jpg') ?>" /></li> 
+		<!-- <li><img src="<?php echo base_url('assets/images/banner/11.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/12.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/13.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/14.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/15.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/16.jpg') ?>" /></li>  
+		<li><img src="<?php echo base_url('assets/images/banner/17.jpg') ?>" /></li>   -->
+		
+
+			<li><img src="<?php echo base_url('assets/images/homebanner.jpg') ?>" /></li>
+			<li><img src="<?php echo base_url('assets/images/home1.jpg') ?>" /></li>
+			<li><img src="<?php echo base_url('assets/images/Library-2.jpg') ?>" /></li>
+			<li><img src="<?php echo base_url('assets/images/Maths-Block.jpg') ?>" /></li>
+			<li><img src="<?php echo base_url('assets/images/MBA-Block-2.jpg') ?>" /></li>
+			
+		</ul>
+	</div>
+</div>
+<!-- Home 01 Banner Ends -->
+<!----------------------------------------------------------------------------------------------------------------------->
+<section class="section-90 section-md-top-111 section-md-bottom-120">
+	<div class="shell-fluid">
+		<div class="range range-xs-center range-md-left text-md-left section-with-images">
+			<div class="cell-xs-8 cell-md-12 cell-lg-8">
+
+				<div class="shell-fluid">
+					<div class="cell-xs-12 cell-sm-12 cell-lg-12">
+						<h2 class="text-bold"></h2>
+						<div class="inset-md-left-36 inset-xl-left-10 inset-xs-left-20 offset-top-45">
+							<h2 class="inset-left-10"><strong>Sri Pushpam History</strong></h2><br>
+							<ul align="justify" class="inset-left-10 list list-marked">
+								<li>It was not a dream but a relative thought of <strong>Sri A.Veeriya Vandayar</strong> and his truthful brother <strong>Sri.A.Krishnaswamy Vandayar</strong> in the year <strong>1956.</strong></li>
+								<li>Started in his residence with <strong>11</strong> students and <strong>3</strong> duty-bound teachers, Sri Pushpam College has now spread its banyan shade for a prosperous learning.</li>
+								<li>Vandayars are gentlemen farmers with a comprehensive skill for administrative management.</li>
+								<li><strong>Sri Pushpam</strong> with its sylvan delta surroundings which has a balanced eco system, is a haven for education.</li>
+							</ul>
+						</div>
+					</div>
+					<div class="cell-xs-12 cell-sm-12 cell-lg-12 section-30">
+						<div class="events fill-height">
+							<div class="range range-condensed">
+								<div class="cell-sm-6">
+									<article>
+										<div class="post-image-wrap"><img alt="" class="img-responsive" height="370" src="<?php echo base_url('assets/images/home_images/alumni.jpg') ?>" width="640" /></div>
+										<div class="post-body">
+											<h4 class="text-bold"><a href="<?php echo base_url('alumni') ?>">Alumni</a></h4>
+										</div>
+									</article>
+								</div>
+								<div class="cell-sm-6">
+									<article>
+										<div class="post-image-wrap"><img alt="" class="img-responsive" height="370" src="<?php echo base_url('assets/images/home_images/gallery.jpg') ?>" width="640" /></div>
+										<div class="post-body">
+											<h4 class="text-bold"><a href="<?php echo base_url('gallery') ?>"  rel="noopener">Photo Gallery</a></h4>
+										</div>
+									</article>
+								</div>
+								<div class="cell-sm-6">
+									<article>
+										<div class="post-image-wrap"><img alt="" class="img-responsive" height="370" src="<?php echo base_url('assets/images/home_images/library.jpg') ?>" width="640" /></div>
+										<div class="post-body">
+											<h4 class="text-bold"><a href="<?php echo base_url('library') ?>" target="_blank" rel="noopener">Library</a></h4>
+										</div>
+									</article>
+								</div>
+								<div class="cell-sm-6">
+									<article>
+										<div class="post-image-wrap"><img alt="" class="img-responsive" height="370" src="<?php echo base_url('assets/images/home_images/placement.jpg') ?>" width="640" /></div>
+										<div class="post-body">
+											<h4 class="text-bold"><a href="<?php echo base_url('principal-message') ?>" target="_blank" rel="noopener">Principal's Message</a></h4>
+										</div>
+									</article>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="cell-xs-4 cell-md-4 cell-lg-4 offset-top-60 range">
+				<div class="first-image-wrap to-front"><img alt="" class="img-responsive" height="450" src="<?php echo base_url('assets/images/home_images/history1.jpg') ?>" width="750" /></div>
+			</div> -->
+			<div class="cell-xs-4 cell-md-12 cell-lg-4">
+				<div class="shell-fluid">
+
+					<h2 class="text-bold">News &amp; Events</h2>
+					<hr>
+					<?php foreach ($datas as $row) { ?>
+					<?php $startdate = strtotime($row->NewsEventsAddedDate);
+						$enddate = strtotime("+5 day", $startdate);
+						$today = strtotime("today");
+						echo "<p>";
+						if ($today <= $enddate) {
+							echo	'<img src="' . base_url('assets/images/new1.gif') . '"  />';
+						}
+						echo '<b>' . $row->NewsEventsTitle . '</b></p>';
+						echo "<p>" . $row->NewsEventsContent . "</p>";
+					}
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="shell-fluid">
+			<div class="range range-xs-center range-md-left text-left">
+				<div class="cell-xs-5 cell-sm-4 cell-md-4"><img alt="" class="img-responsive" height="400" src="<?php echo base_url('assets/images/vision.jpg') ?>" /></div>
+				<div class="cell-xs-12 cell-sm-8 cell-md-8">
+					<h2 class="text-bold">Vision &amp; Mission</h2>
+					<br />
+					<div class="">
+						<p align="justify">Started by Rao Bahadur <strong>Shri.A.Veeriya Vandayar</strong> and his illustrious brother <strong>Shri.A.Krishnaswamy Vandayar</strong> of Poondi with a far-sighted vision of educating the rural masses, the motto of the College is <strong>"to learn and to serve"</strong>.</p>
+						<br /><br /><button class="btn btn-xs btn-primary"><a href="<?php echo base_url('vision-mission') ?>"> Read more</a></button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+</section>
+<!----------------------------------------------------------------------------------------------------------------------->
+<?php
+init_public_tail();
+?>
