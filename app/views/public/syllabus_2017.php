bda638a62e51810ba600440335ec2252cdccb86f
Manikandan R <mani_suja@hotmail.com>
2021-09-28 12:32:18 +0530

    syllabus updated

--- app/views/public/syllabus_2017.php
+++ app/views/public/syllabus_2017.php
@@ -24,118 +24,118 @@ init_public_head();
                     </tr>
                     <tr>
                         <td>Economics</td>
-                        <td><?php syllabus_link('Economics/baeconomics') ?></td>
-						<td><?php syllabus_link('Economics/maeconomics') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/ba_economics_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/ma_economics_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Tamil</td>
-                        <td><?php syllabus_link('Tamil/batamil') ?></td>
-						<td><?php syllabus_link('Tamil/matamil') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/ba_tamil_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/ma_tamil_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Indian Culture</td>
-                        <td><?php syllabus_link('Tamil/baindianculture') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/ba_indianculture_syllabus') ?></td>
                         <td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>B.Lit. Tamil</td>
-                        <td><?php syllabus_link('Tamil/blit') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/ba_literature_syllabus') ?></td>
                         <td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>English</td>
-                        <td><?php syllabus_link('English/baenglish') ?></td>
-						<td><?php syllabus_link('English/maenglish') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/ba_english_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/ma_english_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Commerce</td>
-                        <td><?php syllabus_link('commerce/bcom') ?></td>
-						<td><?php syllabus_link('commerce/mcom') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bcom_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/mcom_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>B. Com. C.A.</td>
-                        <td><?php syllabus_link('commerce/bcomca') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bcom_computerapplication_syllabus') ?></td>
 						<td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>B. Com. B. M.</td>
-                        <td><?php syllabus_link('commerce/bcombm') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bcom_bankmanagement_syllabus') ?></td>
 						<td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>Mathematics</td>
-                        <td><?php syllabus_link('Mathematics/bscmaths') ?></td>
-						<td><?php syllabus_link('Mathematics/mscmaths') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_mathematics_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_mathematics_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Physics</td>
-                        <td><?php syllabus_link('Physics/bscphysics') ?></td>
-						<td><?php syllabus_link('Physics/mscphysics') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_physics_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_physics_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Chemistry</td>
-                        <td><?php syllabus_link('chemistry/bsc') ?></td>
-						<td><?php syllabus_link('chemistry/msc') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_chemistry_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_chemistry_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Botany</td>
-                        <td><?php syllabus_link('botany/bsc') ?></td>
-						<td><?php syllabus_link('botany/msc') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_botany_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_botany_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Microbiology</td>
                         <td class="pdf_items">-</td>
-                        <td><?php syllabus_link('Microbiology/msc') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_microbiology_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Zoology</td>
-                        <td><?php syllabus_link('Zoology/bsczoology') ?></td>
-						<td><?php syllabus_link('Zoology/msczoology') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_zoology_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_zoology_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Biotechnology</td>
                         <td class="pdf_items">-</td>
-                        <td><?php syllabus_link('Zoology/mscbiotech') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_biotechnology_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Computer Science</td>
-                        <td><?php syllabus_link('Computer Science/bsccs') ?></td>
-						<td><?php syllabus_link('Computer Science/msccs') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_computerscience_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_computerscience_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Computer Applications</td>
-                        <td><?php syllabus_link('Computer Science/BCA') ?></td>
-						<td><?php syllabus_link('Computer Science/MCA') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bca_syllabus') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/mca_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Information Technology</td>
                         <td class="pdf_items">-</td>
-                        <td><?php syllabus_link('Computer Science/mscit') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/msc_informationtechnology_syllabus') ?></td>
                     </tr>
                     <tr>
                         <td>Physical Education, Health Education and Sports</td>
-                        <td><?php syllabus_link('Physical Education/bscphysicaleducation') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_physicaleducation_syllabus') ?></td>
 						<td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>Business Administration	</td>
-                        <td><?php syllabus_link('businessadministration/BBA') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bba_syllabus') ?></td>
 						<td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>Statistics</td>
-                        <td><?php syllabus_link('Statistics/bscstatistics') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_statistics_syllabus') ?></td>
                         <td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>Geography</td>
-                        <td><?php syllabus_link('Geography/bscgeography') ?></td>
+                        <td><?php syllabus_link('syllabus_2017_updated/UG_SYLLABUS/bsc_geography_syllabus') ?></td>
                         <td class="pdf_items">-</td>
                     </tr>
                     <tr>
                         <td>M.L.I.S</td>
                         <td class="pdf_items">-</td>
-						<td><?php syllabus_link('Library and Information Science/MLIS') ?></td>
+						<td><?php syllabus_link('syllabus_2017_updated/PG_SYLLABUS/mlis_syllabus') ?></td>
                     </tr>
                     <!-- <tr>
                         <td>Management Studies</td>
