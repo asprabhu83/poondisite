238821e95b8a79540cf476970b842251c4f6aece
Manikandan R <mani_suja@hotmail.com>
2021-09-29 11:10:39 +0530

    principal message updated

--- app/views/public/principal_message.php
+++ app/views/public/principal_message.php
@@ -29,18 +29,20 @@ init_public_head();
 				<!-- <br /><img alt="" class="img-responsive" height="1200" src="<?php echo base_url('assets/images/principal1.jpg') ?>" width="750" /><center> -->
 				<p><strong>Dr. R. Sivakumar <br />Principal</strong></p>
 				</center></center>
+				<div class="rm">
 				<div class="inset-md-left-36 inset-xl-left-110 offset-top-60">
 					<ul class="list list-marked">
-					<p style="color:red;" align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We are proud that Sri Pushpam college, the Temple of Learning, is striding towards excellence in Higher Education in academic, curricular and extra-curricular activities.
+					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  We are proud that Sri Pushpam college, the Temple of Learning, is striding towards excellence in Higher Education in academic, curricular and extra-curricular activities.
 					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; I deem it an honorable prestige to notice our academicians motivating the rural youth, bring professionally qualified and socially committed. Our curriculum and teaching &ndash; learning process focus primarily to mould the younger generation moving higher and higher to achieve their goal in Higher Education.
 
-					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The college very closely on the verge of <strong>Diamond Jubilee</strong> paves the way for the phenomenal growth in physical atmosphere and the academic ambience of this great institution. The unique and phenomenal growth of this glorious institution is all due to the constant support and keen interest shown by the Managing Committee in general and by our revered Secretary and Correspondent, Shriman Ayya in particular.
-					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We are highly indebted to the teaching fraternity for their untiring service, to the administrative machinery for its constant support and to the student community for its insatiable interest and involvement in studiously building this temple of learning of higher education and research.
+					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  The college very closely on the verge of <strong>Diamond Jubilee</strong> paves the way for the phenomenal growth in physical atmosphere and the academic ambience of this great institution. The unique and phenomenal growth of this glorious institution is all due to the constant support and keen interest shown by the Managing Committee in general and by our revered Secretary and Correspondent, Shriman Ayya in particular.
+					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  We are highly indebted to the teaching fraternity for their untiring service, to the administrative machinery for its constant support and to the student community for its insatiable interest and involvement in studiously building this temple of learning of higher education and research.
 
-					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Let us walk straight following the noble thought of our former President of India, A. P. J. Abdul Kalam: &ldquo;My message, especially to young people is to have courage to think differently, courage to invent, to travel the unexplored path, courage to discover the impossible and to conquer the problems and succeed. These are great qualities that they must work towards. This is my message to the young people.&rdquo;
+					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Let us walk straight following the noble thought of our former President of India, A. P. J. Abdul Kalam: &ldquo;My message, especially to young people is to have courage to think differently, courage to invent, to travel the unexplored path, courage to discover the impossible and to conquer the problems and succeed. These are great qualities that they must work towards. This is my message to the young people.&rdquo;
 
-					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Let me conclude my message with Ayya&rsquo;s words: &ldquo;Have an ear for good; it will work for your ambitious bloom.&rdquo;</p>
+					<p align="justify"><span>&#9733;</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Let me conclude my message with Ayya&rsquo;s words: &ldquo;Have an ear for good; it will work for your ambitious bloom.&rdquo;</p>
 					</ul>
+				</div>
 				</div>
 			</div>
 		</div>
