47465df98f2939021492885605eca0292df1f7ba
Manikandan R <mani_suja@hotmail.com>
2021-09-29 11:42:51 +0530

    Certificate updated

--- app/views/public/certificate.php
+++ app/views/public/certificate.php
@@ -5,7 +5,8 @@ init_public_head();
 <div class="container-fluid marqueewrap">
 	<div class="alert alert-primary alert-dismissible" role="alert">
 		<marquee>
-			<strong><span>&#9733;</span> <a href="<?php echo base_url('https://cubonline.biz/StudentLogin?Instname=AVVMSPCP') ?>" target="_blank">Last date for Payment of Tuition Fees on 28.09.2021 at 4.30 p.m.</a><span>&#9733;</span></strong>
+            <strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank">Mid Semester Examination start from 04.10.2021</a><span>&#9733;</span></strong>
+			<!-- <strong><span>&#9733;</span> <a href="<?php echo base_url('https://cubonline.biz/StudentLogin?Instname=AVVMSPCP') ?>" target="_blank">Last date for Payment of Tuition Fees on 28.09.2021 at 4.30 p.m.</a><span>&#9733;</span></strong> -->
 			<!-- <strong>35th Academic Council Meeting on 18.09.2021 at 10.30 a.m.</strong><span>&#9733;</span> -->
 			<!-- <strong><a href="<?php echo base_url('assets/images/founderdaysep2021.jpeg') ?>" target="_blank">51st Founder's Commemoration Day on 17.09.2021</strong></a><span>&#9733;</span> -->
 			<!-- <strong><a href="<?php echo base_url('assets/images/iobpay.jpeg') ?>" target="_blank">Inaugural Function of IOB Pay on 14.09.2021 at 2.30 p.m.</strong></a> -->
@@ -25,7 +26,7 @@ init_public_head();
 					<div class="cell-xs-12 cell-sm-12 cell-lg-12">
 						<h2 class="text-bold"></h2>
 						<div class="inset-md-left-36 inset-xl-left-10 inset-xs-left-20 offset-top-45">
-							<h2 class="inset-left-10"><strong>Certificate </strong></h2><br>
+							<h2 class="inset-left-10"><strong>Certificate of Accreditation</strong></h2><br>
 							
 						</div>
 					</div>
