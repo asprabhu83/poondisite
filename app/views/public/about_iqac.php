944df505365aa95cfa1d26ab676fc771e220c9e4
Manikandan R <mani_suja@hotmail.com>
2021-10-17 23:21:27 +0530

    about iqac updated

--- app/views/public/about_iqac.php
+++ app/views/public/about_iqac.php
@@ -17,9 +17,15 @@ h6    {color: brown;}
     }
           
     body {background-color: powderblue;}
-        h2   {color: brown;}
-        h3    {color: green;}
-        h4    {color: red;} 
+        h1   {text-align: center;color: brown;}
+        h2   {text-align: left;color: brown;}
+        h3    {text-align: left;color: green;}
+        h4    {text-align: left;color: red;} 
+        
+        h2.a {
+            text-align: center;
+            color: red;.
+        }
 
         /* <!-- Element selected by id --> */
         #list{
@@ -34,15 +40,30 @@ h6    {color: brown;}
             padding-left: 20px; */
             text-indent: -26px;
             padding-left: 26px;
+            list-style-position: outside;
         }
   
         /* <!-- Removes default style of bullet point --> */
-        lirm{
+        li{
+            /* list-style-image:URL('https://media.geeksforgeeks.org/wp-content/uploads/20200331172037/image47.png'); */
+            list-style-position: outside;
+        }
+        /* li{
             list-style: none;
+        } */
+         ul{
+            margin: 20px 20px;
+            list-style-position: outside;
+        } 
+        ul.a {
+            list-style-position: outside;
+        }
+        div.a {
+        text-align: left;
         }
   
-        /* <!-- ::before creates a pseudo-element that is the first child of the  selected element --> */
-        lirm::before{
+        /* <!-- ::before creates a pseudo-element         that is the first child of the  selected element --> */
+        li::before{
   
             /* <!-- Unicode for >> character --> */
             content: "\2B9A";
@@ -60,90 +81,94 @@ h6    {color: brown;}
 		</div>
 		<ul class="list-inline list-inline-with-arrows p">
 			<ul class="list-inline list-inline-with-arrows p">
-				<lirm><a href="<?php echo base_url()?>" class="text-white">Home </a></lirm>
-				<lirm>About IQAC</lirm>
+				<li><a href="<?php echo base_url()?>" class="text-white">Home </a></li>
+				<li>About IQAC</li>
 			</ul>
 		</ul>
 	</div>
 </div>
+
 <section class="section-90 section-md-top-120 section-md-bottom-111">
 	<div class="shell-fluid">
-		<h2 class="text-bold">About IQAC</h2>
-		<div class="range range-xs-center range-md-left text-md-left">
+       
+		<h2 class="text-bold a">About IQAC</h2>
+		<!-- <div class="range range-xs-center range-md-left text-md-left"> -->
 			<!-- <div class="cell-xs-4 cell-sm-4 cell-md-4 cell-xl-12 cell-xl-preffix-1"><img alt="" class="img-responsive" height="150" src="<?php echo base_url('assets/images/clg.jpg')?>" width="1500" /></div> -->
-            <div><img alt=""  height="250" src="<?php echo base_url('assets/images/clg.jpg')?>" width="500" /></div>
+            <!-- <div><img alt=""  height="250" src="<?php echo base_url('assets/images/clg.jpg')?>" width="500" /></div> -->
          <br>
             <!-- <div class="cell-xs-4 cell-sm-4 cell-md-4 cell-xl-6 cell-xl-preffix-1"><img alt="" class="img-responsive" height="250" src="<?php echo base_url('assets/images/bank.jpg')?>" width="250" /></div> -->
-			<div class="cell-xs-4 cell-sm-4 cell-md-4 cell-xl-12 cell-xl-preffix-1">
+			<!-- <div class="cell-xs-4 cell-sm-4 cell-md-4 cell-xl-12 cell-xl-preffix-1"> -->
 				<!-- <h3 class="text-bold">Special Features</h3> -->
-				<br />
+     <div class="a"> 
+				
 				<h2 class="text-bold">IQAC – Vision</h2>
-				<br />
+				
 				<ul id="list">
-						<lirm>    To ensure quality culture as the prime concern for the Higher Education Institutions through institutionalizing and internalizing all the initiatives taken with internal and external support.</lirm>
+						<li>    To ensure quality culture as the prime concern for the Higher Education Institutions through institutionalizing and internalizing all the initiatives taken with internal and external support.</li>
 				</ul>
 
-                <br />
+                
 				<h2 class="text-bold">Objective</h2>
-				<br />
+				<br/>
 				<ul id="list">
                 <h3 class="text-bold">The primary aim of IQAC is</h3>
-                <br>
-                        <lirm>    To develop a system for conscious, consistent and catalytic action to improve the academic and administrative performance of the institution.</lirm><br>
-                        <lirm>    To promote measures for institutional functioning towards quality enhancement through internalization of quality culture and institutionalization of best practices.</lirm>
+                
+                        <li>    To develop a system for conscious, consistent and catalytic action to improve the academic and administrative performance of the institution.</li>
+                        <li>    To promote measures for institutional functioning towards quality enhancement through internalization of quality culture and institutionalization of best practices.</li>
                     </ul>
                 
-                <br />
+                
 				<h2 class="text-bold">Strategies</h2>
-				<br />
+				
                 <ul id="list">
                         <h3 class="text-bold">IQAC shall evolve mechanisms and procedures for</h3>
-                        <br>
-                        <lirm>	  Ensuring timely, efficient and progressive performance of academic, administrative and financial tasks;</lirm><br>
-                        <lirm>    Relevant and quality academic/ research programmes;</lirm><br>
-                        <lirm>    Equitable access to and affordability of academic programmes for various sections of society;</lirm><br>
-                        <lirm>    Optimization and integration of modern methods of teaching and learning;</lirm><br>
-                        <lirm>    The credibility of assessment and evaluation process;</lirm><br>
-                        <lirm>    Ensuring the adequacy, maintenance and proper allocation of support structure and services;</lirm><br>
-                        <lirm>    Sharing of research findings and networking with other institutions in India and abroad.</lirm><br>
                         
+                        <li>	Ensuring timely, efficient and progressive performance of academic, administrative and financial tasks;</li>
+                        <li>    Relevant and quality academic/ research programmes;</li>
+                        <li>    Equitable access to and affordability of academic programmes for various sections of society;</li>
+                        <li>    Optimization and integration of modern methods of teaching and learning;</li>
+                        <li>    The credibility of assessment and evaluation process;</li>
+                        <li>    Ensuring the adequacy, maintenance and proper allocation of support structure and services;</li>
+                        <li>    Sharing of research findings and networking with other institutions in India and abroad.</li>
+                
                     </ul>
                 
-                    <br />
+                    
 				<h2 class="text-bold">Functions</h2>
-				<br />
-                <ul id="list">
+				
+                    <ul id="list">
                         <h3 class="text-bold">Some of the functions expected of the IQAC are:</h3>
-                        <br>
-                        <lirm>    Development and application of quality benchmarks</lirm><br>
-                        <lirm>    Parameters for various academic and administrative activities of the institution;</lirm><br>
-                        <lirm>    Facilitating the creation of a learner-centric environment conducive to quality education and faculty maturation to adopt the required knowledge and technology for participatory teaching and learning process;</lirm><br>
-                        <lirm>    Collection and analysis of feedback from all stakeholders on quality-related institutional processes;</lirm><br>
-                        <lirm>    Dissemination of information on various quality parameters to all stakeholders;</lirm><br>
-                        <lirm>    Organization of inter and intra institutional workshops, seminars on quality related themes and promotion of quality circles;</lirm><br>
-                        <lirm>    Documentation of the various programmes/activities leading to quality improvement;</lirm><br>
-                        <lirm>    Acting as a nodal agency of the Institution for coordinating quality-related activities, including adoption and dissemination of best practices;</lirm><br>
-                        <lirm>    Development and maintenance of institutional database through MIS for the purpose of maintaining /enhancing the institutional quality;</lirm><br>
-                        <lirm>    Periodical conduct of Academic and Administrative Audit and its follow-up</lirm><br>
-                        <lirm>    Preparation and submission of the Annual Quality Assurance Report (AQAR) as per guidelines and parameters of NAAC.</lirm><br>
+                        
+                        <li>    Development and application of quality benchmarks</li>
+                        <li>    Parameters for various academic and administrative activities of the institution;</li>
+                        <li>    Facilitating the creation of a learner-centric environment conducive to quality education and faculty maturation to adopt the required knowledge and technology for participatory teaching and learning process;</li>
+                        <li>    Collection and analysis of feedback from all stakeholders on quality-related institutional processes;</li>
+                        <li>    Dissemination of information on various quality parameters to all stakeholders;</li>
+                        <li>    Organization of inter and intra institutional workshops, seminars on quality related themes and promotion of quality circles;</li>
+                        <li>    Documentation of the various programmes/activities leading to quality improvement;</li>
+                        <li>    Acting as a nodal agency of the Institution for coordinating quality-related activities, including adoption and dissemination of best practices;</li>
+                        <li>    Development and maintenance of institutional database through MIS for the purpose of maintaining /enhancing the institutional quality;</li>
+                        <li>    Periodical conduct of Academic and Administrative Audit and its follow-up</li>
+                        <li>    Preparation and submission of the Annual Quality Assurance Report (AQAR) as per guidelines and parameters of NAAC.</li>
                                                
                     </ul>
 
-                    <br />
+                    
 				<h2 class="text-bold">Benefits</h2>
-				<br />
-                <ul id="list">
+				
+                    <ul id="list">
                         <h3 class="text-bold">IQAC will facilitate / contribute to</h3>
-                        <br>
-                        <lirm>    Ensure clarity and focus in institutional functioning towards quality enhancement;</lirm><br>
-                        <lirm>    Ensure internalization of the quality culture;</lirm><br>
-                        <lirm>    Ensure enhancement and coordination among various activities of the institution and institutionalize all good practices;</lirm><br>
-                        <lirm>    Provide a sound basis for decision-making to improve institutional functioning;</lirm><br>
-                        <lirm>    Act as a dynamic system for quality changes in HEIs;</lirm><br>
-                        <lirm>    Build an organised methodology of documentation and internal communication.</lirm><br>
+                        
+                        <li>    Ensure clarity and focus in institutional functioning towards quality enhancement;</li>
+                        <li>    Ensure internalization of the quality culture;</li>
+                        <li>    Ensure enhancement and coordination among various activities of the institution and institutionalize all good practices;</li>
+                        <li>    Provide a sound basis for decision-making to improve institutional functioning;</li>
+                        <li>    Act as a dynamic system for quality changes in HEIs;</li>
+                        <li>    Build an organised methodology of documentation and internal communication.</li>
+                                                
                     </ul>
-			</div>
-		</div>
+			<!-- </div> -->
+		<!-- </div> -->
 		<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------>
 			
 		</div>
