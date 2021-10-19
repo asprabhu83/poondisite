577fb93e4421e6326f1c1801de9c83b0e5acf84c
Manikandan R <mani_suja@hotmail.com>
2021-10-19 12:54:27 +0530

    message deleted

--- app/views/public/index.php
+++ app/views/public/index.php
@@ -5,10 +5,10 @@ init_public_head();
 <div class="container-fluid marqueewrap">
 	<div class="alert alert-primary alert-dismissible" role="alert">
 		<marquee>
-		<strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> Semester exam commences on 6.12.21 and ends on 31.12.21</a></strong>
+<!-- <strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> Semester exam commences on 6.12.21 and ends on 31.12.21</a></strong>
 <strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> End semester  begins from 22.11.21</a></strong>
 <strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> 10.11.21 is the last date for remitting examination fee without fine.</a></strong>
-<strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> 16.11.21 is the last date for remitting examination fee with fine.</a><span>&#9733;</span></strong>
+<strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> 16.11.21 is the last date for remitting examination fee with fine.</a><span>&#9733;</span></strong> -->
 			<!-- <strong>35th Academic Council Meeting on 18.09.2021 at 10.30 a.m.</strong><span>&#9733;</span> -->
 			<!-- <strong><a href="<?php echo base_url('assets/images/founderdaysep2021.jpeg') ?>" target="_blank">51st Founder's Commemoration Day on 17.09.2021</strong></a><span>&#9733;</span> -->
 			<!-- <strong><a href="<?php echo base_url('assets/images/iobpay.jpeg') ?>" target="_blank">Inaugural Function of IOB Pay on 14.09.2021 at 2.30 p.m.</strong></a> -->
