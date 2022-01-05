21c2e6845ad0550e656f7ae97c8523fe68084dcb
manikandan540 <mani_suja@hotmail.com>
2022-01-05 21:04:22 +0530

    exam time table updated

--- app/views/public/index.php
+++ app/views/public/index.php
@@ -5,7 +5,9 @@ init_public_head();
 <div class="container-fluid marqueewrap">
 	<div class="alert alert-primary alert-dismissible" role="alert">
 		<marquee>
-		<strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> Last date for the Remitatance of Odd Semester Examination fee without fine 09.12.2021  and with Fine Rs.100 /- 14.12.2021.</a><span>&#9733;</span></strong>
+		<strong><a href="<?php echo base_url('pdf/Exam/UG_Time_Table_Nov_2021.pdf') ?>" target="_blank">Semester Examination Time Table for Nov 2021(UG).pdf</strong></a><span>&#9733;</span>
+		
+		<!-- <strong><span>&#9733;</span> <a href="<?php echo base_url('#') ?>" target="_blank"> Last date for the Remitatance of Odd Semester Examination fee without fine 09.12.2021  and with Fine Rs.100 /- 14.12.2021.</a><span>&#9733;</span></strong> -->
 		<!-- <strong><span>&#9733;</span><a href="<?php echo base_url('#') ?>" target="_blank">SSR Submitted on 20.10.2021 </strong></a><span>&#9733;</span> 	
 		<strong><a href="<?php echo base_url('#') ?>" target="_blank">NAAC Initiated SSS  - Respond immediately</strong></a><span>&#9733;</span> 	
 		<strong><a href="<?php echo base_url('assets/images/16th_graduation_day.jpeg') ?>" target="_blank">16 <sup>th</sup> GRADUATION DAY (GIRLS) ON 30.10.2021 </strong></a><span>&#9733;</span> 
