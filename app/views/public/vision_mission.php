abc88947f201c96a827bbb0c09078c120a58281b
manikandan540 <mani_suja@hotmail.com>
2022-02-18 09:56:58 +0530

    update vision mission

--- app/views/public/vision_mission.php
+++ app/views/public/vision_mission.php
@@ -24,12 +24,24 @@ init_public_head();
 				<ul class="inset-left-10 list list-marked">
 					<div class="inset-md-left-30 offset-top-10">
 						
+						<div class="rm">
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The vision of the college shoots from <strong> Rao Bahadur Sri A. Veeriya Vandayar </strong> and his eminent brother <strong> Sri A. Krishnaswamy Vandayar </strong> of Poondi is to educate the rural masses and elevate the living condition of the people of delta region. 
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The motto of our institution is ‘To Learn and To Serve’ which is based on the age-old Indian Philosophy that the very purpose of learning is to serve the society. </li>
+							</li>
+							<br><br>
+							<p text-align="justify">	<strong>The objectives are as follows. </strong> 
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  To achieve quality academic programmes and value oriented higher education to the rural community.  
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  To promote academic excellence in teaching, learning and research. 
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  To equip them to meet the challenges in terms of regional, national and global demands with moral standards and quality.  
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  To raise the level of socially conscious citizens. To foster the sense of moral responsibility. 
+							</p>
+						</div>
+
 							<!-- <p align="justify">Started by Rao Bahadur <strong>Shri.A.Veeriya Vandayar</strong> and his illustrious brother <strong>Shri.A.Krishnaswamy Vandayar</strong> of Poondi with a far-sighted vision of educating the rural masses, the motto of the College is <strong>"to learn and to serve"</strong>. The Secretary and Correspondent of the college, <strong>Shri.K.Thulasiah Vandayar</strong> has a vision to offer quality academic programmes and value oriented higher education to the rural community in Tamilnadu to equip them to meet the challenges of time in terms of regional, national and global demands with moral standards and quality. -->
 							<!-- <rol><rli></rli><p text-align="justify">	<span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The vision of the college shoots from <strong> Rao Bahadur Sri A. VeeriyaVandayar</strong> and his eminent brother <strong>Sri A. KrishnaswamyVandayar</strong> of Poondi is to educate the rural masses and elevate the living condition of the people of delta region. </li>
 							</rli></rol> -->
-							<div class="rm">
+							<!-- <div class="rm">
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The vision of the college shoots from <strong> Rao Bahadur Sri A. VeeriyaVandayar</strong> and his eminent brother <strong>Sri A. KrishnaswamyVandayar</strong> of Poondi is to educate the rural masses and elevate the living condition of the people of delta region.
-							
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; In keeping with the vision the college, it works towards making education purposeful for the society. </li>
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	The motto of the college is ‘To Learn and To Serve’.</li>
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	The motto unfolds out of the philosophy ‘come to team and go to serve’. </li>
@@ -41,7 +53,8 @@ init_public_head();
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  To equip them to meet the challenges in terms of regional, national and global demands with moral standards and quality.  
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  To raise the level of socially conscious citizens. 
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  To foster the sense of moral responsibility.
-							</p></div>
+							</p></div> -->
+
 						
 						</p>
 					</div>
@@ -71,13 +84,13 @@ init_public_head();
 						</li> -->
 
 						<div class="rm">
-							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong> In Keeping with the vision of AVVMSPC, it steps with its mission; “We endeavor to shape self-empowered individuals for the growth of dynamic and vibrant society.”</strong>
-						
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong> In keeping with its vision, AVVMSPC steps with its mission; “We endeavor to shape self-empowered individuals for the growth of dynamic and vibrant society” </strong>
+
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; It is open-eyed in providing affordable education and offering value-based academic programmes to contribute to national development in terms of inclusion of all sections of the society from this delta region.
 							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; It inculcates students with the values of discipline, team spirit, hard work and scientific tempers to meet local, national and global challenges.
-							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; The institution is fully conscious to provide platform to actualize students’ talents and to mould their passion into profession.
-							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; It creates conducive environment for teaching-learning process and innovative research.
-							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; It stands on its own to serve society by extending needful outreach programmes.
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; It is fully conscious to provide a platform to actualize students’ talents and to mould their passion into profession. 
+							<p> <span>&#9733;</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; It creates conducive environment for teaching-learning process and innovative research. It stands on its own to serve society by extending needful outreach programmes.
+							
 							</p>
 						</div>
 					</div>
