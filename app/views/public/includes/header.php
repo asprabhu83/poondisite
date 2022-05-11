9d420130bd344fd3d826685ad7eb20fa566f2bb1
manikandan540 <mani_suja@hotmail.com>
2022-05-11 05:58:19 +0530

    AQAR updated

--- app/views/public/includes/header.php
+++ app/views/public/includes/header.php
@@ -382,7 +382,8 @@ ENVIRONMENT == 'development' ?  ob_start() : '';
                                                         </li>   
                                                         <li class="active "><a href="https://naac.sripushpamcollege.co.in/" target="_blank"><span>NAAC-AVVMSPC Portal</span></a>
                                                         </li>   
-                                                        
+                                                        <li class="active "><a href="https://aqar2021.sripushpamcollege.co.in/" target="_blank"><span>AQAR-2021</span></a>
+                                                        </li>   
                                                        
                                                     <!-- <li class="active"><a href="<?php echo base_url('certificate') ?>"><span>NAAC Certificate</span></a>
                                                         </li> -->
