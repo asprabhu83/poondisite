c254b823ae646373ff042c464ac0ef6e458485d5
manikandan540 <mani_suja@hotmail.com>
2022-07-25 21:53:07 +0530

    MBA updated

--- app/views/public/spcte.php
+++ app/views/public/spcte.php
@@ -72,33 +72,10 @@ init_public_head();
 
 			<div class="cell-xs-10 cell-sm-6 cell-md-3">
 				<article class="post post-event"><img alt="" class="img-responsive" height="100" src="<?php echo base_url('assets/images/MBA_Broucher2.jpeg') ?>" width="100" />
-					<!-- <h3 style="color:Blue;" align="center" class="text-bold">MBA Programme</h3> -->
-					<!-- <p><h5 style="color:green;" align="center" class="text-bold">Coordinator and Associate Professor, <br> Department of Physics</h5><strong></strong></p> -->
 				</article>
-				<!-- <h4 style="color:red;" align="center"  class="text-bold"> Figured in the list of <br> World top 2% Scientists released by <br> </h4> <h4 style="color:brown;" align="center"  class="text-bold"> "Stanford Universtiry, USA" </h4>	
-				<br>
-				 <h4 style="color:red;" align="center"  class="text-bold"> Published 100(Hundred) research articles</h4> <h4 style="color:brown;" align="center"  class="text-bold">in SCI journals within a decade.</h4>	
-				  -->
 			</div>
-			
-			<!-- 
-			<div class="cell-xs-10 cell-sm-6 cell-md-3">
-				<article class="post post-event"><img alt="" class="img-responsive" height="130" src="<?php echo base_url('assets/images/MBA_Broucher2.JPG') ?>" width="270" />
-					<h3 style="color:Blue;" align="center" class="text-bold">MBA Programme </h3>
-					<p><h5 style="color:green;" align="center" class="text-bold">Associate Professor, <br>Department of Botany</h5><strong></strong></p> 
-				</article>
-				 <h4 style="color:red;" align="center"  class="text-bold"> Awaded D.Sc.,  <br></h4> <h4 style="color:brown;" align="center"  class="text-bold">“Diversity and Bioprospecting  of microflora in marine and terrestrial environment” </h4>
-				<h4 style="color:red;" align="center"  class="text-bold"> </h4><br> <br><br> 
-			</div>  -->
-			
 		</div>
 		<br />
-		
-		<!-- <h4 style="color:red;" align="center"  class="text-bold"> Has submitted D.Sc., thesis “Diversity and Bioprospecting  of microflora in marine and terrestrial environment”" </h4> -->
-        <!-- <h4 style="color:red;" align="center"  class="text-bold">Dr.K.Ravichandran, Associate Professor of PG & Research Department of Physics has figured in the list of Wordl top 2% Scientist by "Stanford Universtiry, USA" </h4> -->
-		<!-- <p align="left" class="offset-md-top-20">has figured in the list of Wordl top 2% Scientist by "Stanford Universtiry, USA" </p> -->
-		<!-- <h4 align="left" class="text-bold">UNIVERSITY REPRESENTATIVE</h4>
-		<p align="left" class="offset-md-top-20">Dr. U.Alibava, Prof. &amp; Head, Dept. of Tamil Studies, Bharathidasan University, Trichirappalli - 24.</p> -->
 	</div>
 </section>
 
@@ -110,32 +87,14 @@ init_public_head();
 
 			<div class="cell-xs-10 cell-sm-6 cell-md-3">
 				<article class="post post-event"><img alt="" class="img-responsive" height="100" src="<?php echo base_url('assets/images/MBA_Broucher1.jpeg') ?>" width="100" />
-					<!-- <h3 style="color:Blue;" align="center" class="text-bold">MBA Programme</h3> -->
-					<!-- <p><h5 style="color:green;" align="center" class="text-bold">Coordinator and Associate Professor, <br> Department of Physics</h5><strong></strong></p> -->
+					
 				</article>
-				<!-- <h4 style="color:red;" align="center"  class="text-bold"> Figured in the list of <br> World top 2% Scientists released by <br> </h4> <h4 style="color:brown;" align="center"  class="text-bold"> "Stanford Universtiry, USA" </h4>	
-				<br>
-				 <h4 style="color:red;" align="center"  class="text-bold"> Published 100(Hundred) research articles</h4> <h4 style="color:brown;" align="center"  class="text-bold">in SCI journals within a decade.</h4>	
-				 -->
+				
 			</div>
-						
-			 <!-- <div class="cell-xs-10 cell-sm-6 cell-md-3">
-				<article class="post post-event"><img alt="" class="img-responsive" height="130" src="<?php echo base_url('assets/images/MBA_Broucher2.JPG') ?>" width="270" />
-					<h3 style="color:Blue;" align="center" class="text-bold">MBA Programme </h3>
-					<p><h5 style="color:green;" align="center" class="text-bold">Associate Professor, <br>Department of Botany</h5><strong></strong></p> 
-				</article>
-				 <h4 style="color:red;" align="center"  class="text-bold"> Awaded D.Sc.,  <br></h4> <h4 style="color:brown;" align="center"  class="text-bold">“Diversity and Bioprospecting  of microflora in marine and terrestrial environment” </h4>
-				<h4 style="color:red;" align="center"  class="text-bold"> </h4><br> <br><br>
-			</div>  -->
-			
+							
 		</div>
 		<br />
-		
-		<!-- <h4 style="color:red;" align="center"  class="text-bold"> Has submitted D.Sc., thesis “Diversity and Bioprospecting  of microflora in marine and terrestrial environment”" </h4> -->
-        <!-- <h4 style="color:red;" align="center"  class="text-bold">Dr.K.Ravichandran, Associate Professor of PG & Research Department of Physics has figured in the list of Wordl top 2% Scientist by "Stanford Universtiry, USA" </h4> -->
-		<!-- <p align="left" class="offset-md-top-20">has figured in the list of Wordl top 2% Scientist by "Stanford Universtiry, USA" </p> -->
-		<!-- <h4 align="left" class="text-bold">UNIVERSITY REPRESENTATIVE</h4>
-		<p align="left" class="offset-md-top-20">Dr. U.Alibava, Prof. &amp; Head, Dept. of Tamil Studies, Bharathidasan University, Trichirappalli - 24.</p> -->
+				
 	</div>
 </section>
 
