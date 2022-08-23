d068a6c64c9fb4b6a53353a69f4b55d7df22e96e
manikandan540 <mani_suja@hotmail.com>
2022-08-23 20:10:55 +0530

    MCA updated

--- app/views/public/spcte_core_faculty.php
+++ app/views/public/spcte_core_faculty.php
@@ -7,7 +7,7 @@ init_public_head();
     <div class="offset-top-20"></div>
     <div class="offset-top-20"><br />
 
-        <h4 align="" class="text-bold offset-top-30" id="q-3">CORE FACULTY MEMBERS</h4>
+        <h4 align="" class="text-bold offset-top-30" id="q-3">CORE FACULTY MEMBERS (MBA)</h4>
         <br />
         <table border="1px" cellpadding="3" cellspacing="5" height="100" style="width: 100%;" width="70">
             <tbody></tbody>
@@ -51,6 +51,58 @@ init_public_head();
         </table>
     </div>
 </div>
+
+<div class="cell-xs-8 cell-lg-8">
+    <!-- <h2 class="text-bold text-center">Faculty</h2> -->
+    <div class="offset-top-20"></div>
+    <div class="offset-top-20"><br />
+
+        <h4 align="" class="text-bold offset-top-30" id="q-3">CORE FACULTY MEMBERS (MCA)</h4>
+        <br />
+        <table border="1px" cellpadding="3" cellspacing="5" height="100" style="width: 100%;" width="70">
+            <tbody></tbody>
+            <tbody>
+                <tr>
+                    <th style="text-align: center;">Name of the Faculty Member</th>
+                    <th style="text-align: center;">Designation</th>
+                </tr>
+                <tr>
+                    <!-- <th colspan="3">Aided Faculty</th> -->
+                </tr>
+                <tr>
+									<th><a href="#" target="_blank" rel="noopener">Dr.V.Maniraj</a></th>
+									<td style="text-align: center;">Associate Professor & HOD</td>
+				</tr> 
+                <tr>
+                    <th><a href="<?php echo base_url('spcte_core_faculty') ?>" target="_blank" rel="noopener">Dr.D.Regupathy</a></th>
+                    <td style="text-align: center;">Assistant Professor</td>
+                </tr>
+                <tr>
+                    <th><a href="<?php echo base_url('spcte_core_faculty') ?>" target="_blank" rel="noopener">Dr.K.Sudhakar</a></th>
+                    <td style="text-align: center;">Assistant Professor</td>
+                </tr>
+                <tr>
+                    <th><a href="<?php echo base_url('spcte_core_faculty') ?>" target="_blank" rel="noopener">Dr.V.Ramanathan</a></th>
+                    <td style="text-align: center;">Assistant Professor</td>
+                </tr>
+                <tr>
+                    <th><a href="<?php echo base_url('spcte_core_faculty') ?>" target="_blank" rel="noopener">Mrs. G.Hemamalini</a></th>
+                    <td style="text-align: center;">Assistant Professor</td>
+                </tr>
+                <!-- <tr>
+                    <th><a href="<?php echo base_url('spcte_core_faculty') ?>" target="_blank" rel="noopener">Mrs.B.Mala</a></th>
+                    <td style="text-align: center;">Assistant Professor</td>
+                </tr> -->
+            </tbody>
+        </table>
+    </div>
+</div>
+<p>
+<p>
+<p>
+<p>
+<p>
+<p>
 <?php
 init_public_tail();
 ?>
