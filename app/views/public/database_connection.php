bf99ca50c6ca63284ab3a35566bf97f933d7b52a
manikandan540 <mani_suja@hotmail.com>
2023-03-07 11:25:40 +0530

    db updated

--- app/views/public/database_connection.php
+++ app/views/public/database_connection.php
@@ -5,15 +5,15 @@
 // $password = "";
 // $dbname = "academic";
 
-// $servername = "localhost";
-// $username = "admin_sripushpamc";
-// $password = "7d#yfS47";
-// $dbname = "admin_sripushpamc";
-
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "admin_sripushpamc";
+$password = "7d#yfS47";
 $dbname = "admin_sripushpamc";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "admin_sripushpamc";
 
    $db = mysqli_connect($servername, $username, $password, $dbname);
 
