859a01195aec73e4c9397168e43f9bf4ef05dc57
manikandan540 <mani_suja@hotmail.com>
2023-03-09 22:54:23 +0530

    pushpamala updated

--- app/views/public/includes/header.php
+++ app/views/public/includes/header.php
@@ -78,7 +78,7 @@ ENVIRONMENT == 'development' ?  ob_start() : '';
                                     <ul class="list-inline list-inline-sm">
                                         <li><a href="https://www.facebook.com/spc.poondi/" target="_blank" class="icon fa fa-facebook icon-silver"></a></li>
                                         <li><a href="https://www.youtube.com/channel/UCXrJLcudKedf1LN3uQeZGUQ" target="_blank" class="icon fa fa-youtube-play icon-silver"></a></li>
-                                        <li><a href="https://www.youtube.com/watch?v=X_3fXk7PEdc" target="_blank" class="icon fa fa-youtube-play icon-silver"></a></li>
+                                        <!-- <li><a href="https://www.youtube.com/watch?v=X_3fXk7PEdc" target="_blank" class="icon fa fa-youtube-play icon-silver"></a></li> -->
                                         <li><a href="https://virtualtourcreator.in/360demo/poondi/" target="_blank">Virtual Tour</a></li>
                                         <li><a href="https://www.linkedin.com/school/a.v.v.m.-sri-pushpam-college-poondi---613-503./" target="_blank" class="icon fa fa-linkedin-square icon-silver"></a></li>
                                         <li><a href="https://webmail.sripushpamcollege.co.in/" target="_blank" >Webmail</a></li>
